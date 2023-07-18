d6bc6fdbc753d95fb275873c9f3df8b8924b8732
sahitigo <sahitigopal20@gmail.com>
2023-07-18 21:55:07 +0530

    Update mlops.py

--- mlops.py
+++ mlops.py
@@ -1,9 +1,34 @@
 # app.py
 import streamlit as st
+import seaborn as sns
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, f1_score, recall_score
+from sklearn.model_selection import train_test_split
 
-def main():
-    st.title("My Streamlit App")
-    st.write("Welcome to my app!")
+st.title("Diamond Classifier")
+st.write("Welcome to the Diamond Classifier app!")
 
-if __name__ == '__main__':
-    main()
+# Load the diamond dataset
+diamonds = sns.load_dataset('diamonds')
+
+# Preprocess the data
+X = diamonds.drop('cut', axis=1)
+y = diamonds['cut']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train the model
+if st.button("Train Model"):
+    clf = RandomForestClassifier(random_state=42)
+    clf.fit(X_train, y_train)
+    st.write("Model trained successfully!")
+
+    # Evaluate the model
+    y_pred = clf.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred, average='weighted')
+    sensitivity = recall_score(y_test, y_pred, average='weighted')
+
+    st.write("Evaluation Results:")
+    st.write(f"Accuracy: {accuracy}")
+    st.write(f"F1 Score: {f1}")
+    st.write(f"Sensitivity: {sensitivity}")
