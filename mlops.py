c1b60c150209219c3a463e4e89b9c213e5f703b3
sahitigo <sahitigopal20@gmail.com>
2023-07-18 22:11:10 +0530

    Update mlops.py

--- mlops.py
+++ mlops.py
@@ -1,6 +1,8 @@
 # app.py
 import streamlit as st
+import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, f1_score, recall_score
 from sklearn.model_selection import train_test_split
@@ -21,7 +23,7 @@ def main():
     st.write("Welcome to the Diamond Classifier app!")
 
     # Load the diamond dataset
-    diamonds = sns.load_dataset('diamonds')
+    diamonds = sns.load_dataset('diamonds').copy()
 
     # Preprocess the data
     X = diamonds.drop('cut', axis=1)
@@ -36,6 +38,20 @@ def main():
     st.write(f"Accuracy: {accuracy}")
     st.write(f"F1 Score: {f1}")
     st.write(f"Sensitivity: {sensitivity}")
+
+    # Visualize feature importances
+    importance = model.feature_importances_
+    feature_names = X.columns
+    feature_importance_df = pd.DataFrame({'Feature': feature_names, 'Importance': importance})
+    feature_importance_df = feature_importance_df.sort_values('Importance', ascending=False)
+
+    st.write("Feature Importances:")
+    fig, ax = plt.subplots()
+    sns.barplot(data=feature_importance_df, x='Importance', y='Feature', ax=ax)
+    ax.set_title("Feature Importances")
+    ax.set_xlabel("Importance")
+    ax.set_ylabel("Feature")
+    st.pyplot(fig)
 
     # User input for prediction
     st.write("Make a Prediction")
@@ -53,8 +69,12 @@ def main():
     input_df = pd.DataFrame(input_data, columns=['carat', 'depth', 'table', 'x', 'y', 'z', 'color', 'clarity'])
 
     # Make prediction
-    prediction = model.predict(input_df)[0]
-    st.write("Prediction:", prediction)
+    prediction = None
+    if carat and depth and table and x and y and z and color and clarity:
+        prediction = model.predict(input_df)[0]
+
+    if prediction is not None:
+        st.write("Prediction:", prediction)
 
 if __name__ == '__main__':
     main()
