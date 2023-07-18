bd64c4fd16842a77af51bfa54081a0358d85a122
sahitigo <sahitigopal20@gmail.com>
2023-07-18 22:17:50 +0530

    Update mlops.py

--- mlops.py
+++ mlops.py
@@ -1,80 +1,70 @@
 # app.py
 import streamlit as st
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, f1_score, recall_score
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
 def train_and_evaluate_model(X_train, y_train, X_test, y_test):
-    clf = RandomForestClassifier(random_state=42)
-    clf.fit(X_train, y_train)
+    model = LinearRegression()
+    model.fit(X_train, y_train)
     
-    y_pred = clf.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred, average='weighted')
-    sensitivity = recall_score(y_test, y_pred, average='weighted')
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
     
-    return clf, accuracy, f1, sensitivity
+    return model, mse
+
+def preprocess_input(input_data):
+    # Convert color to numeric codes
+    color_map = {'D': 0, 'E': 1, 'F': 2, 'G': 3, 'H': 4, 'I': 5, 'J': 6}
+    
+    input_data['color'] = input_data['color'].map(color_map)
+    
+    return input_data
 
 def main():
-    st.title("Diamond Classifier")
-    st.write("Welcome to the Diamond Classifier app!")
+    st.title("Diamond Price Estimator")
+    st.write("Welcome to the Diamond Price Estimator app!")
 
     # Load the diamond dataset
-    diamonds = sns.load_dataset('diamonds').copy()
+    diamonds = pd.read_csv('diamonds.csv')
 
     # Preprocess the data
-    X = diamonds.drop('cut', axis=1)
-    y = diamonds['cut']
+    X = diamonds.drop('price', axis=1)
+    y = diamonds['price']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # Train the model
-    model, accuracy, f1, sensitivity = train_and_evaluate_model(X_train, y_train, X_test, y_test)
+    model, mse = train_and_evaluate_model(X_train, y_train, X_test, y_test)
         
     st.write("Model trained successfully!")
     st.write("Evaluation Results:")
-    st.write(f"Accuracy: {accuracy}")
-    st.write(f"F1 Score: {f1}")
-    st.write(f"Sensitivity: {sensitivity}")
-
-    # Visualize feature importances
-    importance = model.feature_importances_
-    feature_names = X.columns
-    feature_importance_df = pd.DataFrame({'Feature': feature_names, 'Importance': importance})
-    feature_importance_df = feature_importance_df.sort_values('Importance', ascending=False)
-
-    st.write("Feature Importances:")
-    fig, ax = plt.subplots()
-    sns.barplot(data=feature_importance_df, x='Importance', y='Feature', ax=ax)
-    ax.set_title("Feature Importances")
-    ax.set_xlabel("Importance")
-    ax.set_ylabel("Feature")
-    st.pyplot(fig)
+    st.write(f"Mean Squared Error: {mse}")
 
     # User input for prediction
-    st.write("Make a Prediction")
+    st.write("Make a Price Prediction")
     carat = st.number_input("Carat:")
+    cut = st.selectbox("Cut:", ['Fair', 'Good', 'Very Good', 'Premium', 'Ideal'])
+    color = st.selectbox("Color:", ['D', 'E', 'F', 'G', 'H', 'I', 'J'])
+    clarity = st.selectbox("Clarity:", ['I1', 'SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF'])
     depth = st.number_input("Depth:")
     table = st.number_input("Table:")
     x = st.number_input("x:")
     y = st.number_input("y:")
     z = st.number_input("z:")
-    color = st.selectbox("Color:", ['D', 'E', 'F', 'G', 'H', 'I', 'J'])
-    clarity = st.selectbox("Clarity:", ['IF', 'VVS1', 'VVS2', 'VS1', 'VS2', 'SI1', 'SI2', 'I1'])
 
     # Preprocess the input
-    input_data = [[carat, depth, table, x, y, z, color, clarity]]
-    input_df = pd.DataFrame(input_data, columns=['carat', 'depth', 'table', 'x', 'y', 'z', 'color', 'clarity'])
+    input_data = pd.DataFrame([[carat, cut, color, clarity, depth, table, x, y, z]],
+                              columns=['carat', 'cut', 'color', 'clarity', 'depth', 'table', 'x', 'y', 'z'])
+    input_data = preprocess_input(input_data)
 
     # Make prediction
     prediction = None
-    if carat and depth and table and x and y and z and color and clarity:
-        prediction = model.predict(input_df)[0]
+    if not input_data.empty:
+        prediction = model.predict(input_data)[0]
 
     if prediction is not None:
-        st.write("Prediction:", prediction)
+        st.write("Predicted Price:", prediction)
 
 if __name__ == '__main__':
     main()
