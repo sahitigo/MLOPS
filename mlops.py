5203e644fc5a77e0a5f3278d86c3bda37d3d1b62
sahitigo <sahitigopal20@gmail.com>
2023-07-19 05:33:58 +0530

    Update mlops.py

--- mlops.py
+++ mlops.py
@@ -1,13 +1,31 @@
 import streamlit as st
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_percentage_error
 
-# Create a title and header
-st.title("My Streamlit App")
-st.header("Welcome!")
+# Load the Boston housing dataset
+boston = load_boston()
+X = boston.data
+y = boston.target
 
-# Add a text input field
-name = st.text_input("Enter your name", "")
+# Split the dataset into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Display a button
-if st.button("Submit"):
-    st.write(f"Hello, {name}!")
+# Train a linear regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
 
+# Predict on the training and test sets
+y_train_pred = model.predict(X_train)
+y_test_pred = model.predict(X_test)
+
+# Calculate MAPE for training and test sets
+train_mape = mean_absolute_percentage_error(y_train, y_train_pred) * 100
+test_mape = mean_absolute_percentage_error(y_test, y_test_pred) * 100
+
+# Display the results
+st.title("Linear Regression Model Evaluation")
+st.header("Dataset: Boston Housing")
+st.write("Train MAPE:", train_mape)
+st.write("Test MAPE:", test_mape)
